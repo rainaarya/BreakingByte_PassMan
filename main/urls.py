67839424d2b98042072a53d1525e84a20fefa4f3
Aryaman Raina <aryaman20034@iiitd.ac.in>
2023-04-19 16:57:35 +0530

    completed generate password

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sign-up/', views.sign_up, name='sign-up'),
     path('sign-out/', views.sign_out, name='sign-out'),
+    path('generate/', views.generate, name='generate')
 ]
--- main/views.py
+++ main/views.py
@@ -8,6 +8,27 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+import random
+import string
+
+def generate_password(length, uppercase, lowercase, special_chars, numbers):
+    characters = ''
+    if uppercase:
+        characters += string.ascii_uppercase
+    if lowercase:
+        characters += string.ascii_lowercase
+    if special_chars:
+        characters += string.punctuation
+    if numbers:
+        characters += string.digits
+
+    if characters == '':
+        print('Error: No characters selected for password generation.')
+        return "Error: No characters selected for password generation."
+    else:
+        password = ''.join(random.choice(characters) for i in range(length))
+        return password
+
 
 # Create your views here.
 
@@ -47,3 +68,15 @@ def sign_up(request):
 def sign_out(request):
     logout(request)
     return redirect('home')
+
+def generate(request):
+    if request.method == 'POST':
+        length = request.POST.get('length')
+        uppercase = request.POST.get('uppercase')
+        lowercase = request.POST.get('lowercase')
+        numbers = request.POST.get('numbers')
+        special_chars = request.POST.get('symbols')
+        password = generate_password(int(length), uppercase, lowercase, special_chars, numbers)    
+        return render(request, 'main/generate.html', {'password': password, 'length': length, 'uppercase': uppercase, 'lowercase': lowercase, 'numbers': numbers, 'special_chars': special_chars})
+
+    return render(request, 'main/generate.html')
