7555bd0b5a9ec34428b3f9a98e7a70a07d7f583d
Aryaman Raina <aryaman20034@iiitd.ac.in>
2023-04-27 18:19:00 +0530

    error handling for ui

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+
+    email = forms.EmailField(required=True)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
